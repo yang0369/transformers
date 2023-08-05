48dc41946962dfa4683058893db4b734647c4dbe
ydshieh <ydshieh@users.noreply.github.com>
2023-08-05 10:13:52 +0200

    .circleci/create_circleci_config.py

--- tempx.py
+++ tempx.py
@@ -6,6 +6,12 @@ import glob
 
 
 if __name__ == "__main__":
+
+    import numpy as np
+    import torch
+    from PIL import Image
+    import transformers
+
     # pytest.main(["-n", "8", "--max-worker-restart", "1", "--dist", "loadfile",  "tests/models/bert/test_modeling_bert.py", "tests/models/gpt2/test_modeling_gpt2.py", "tests/models/bart/test_modeling_bart.py", "tests/models/plbart/test_modeling_plbart.py", "tests/models/mbart/test_modeling_mbart.py", "tests/models/t5/test_modeling_t5.py", "tests/models/big_bird/test_modeling_big_bird.py", "tests/models/gpt_neox/test_modeling_gpt_neox.py"])
 
     test_list_items = sorted([x for x in glob.glob("tests/models/*/**.py") if "test_" in x])
